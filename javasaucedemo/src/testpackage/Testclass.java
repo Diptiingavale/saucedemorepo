f3c9c722d01b71783ffafbe98b1d313e16ce3b12
Diptiingavale <127374765+Diptiingavale@users.noreply.github.com>
2023-03-18 22:37:27 +0530

    FIRST COMMIT

---
+++ javasaucedemo/src/testpackage/Testclass.java
@@ -0,0 +1,11 @@
+package testpackage;
+
+public class Testclass {
+
+	public static void main(String[] args) {
+		System.out.println(" TESTcase CLASS");
+		System.out.println(" UTIL");
+		System.out.println(" POM.XML");
+	}
+
+}
