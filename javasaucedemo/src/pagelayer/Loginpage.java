ba65ddb4d2e52cf62da077b0f7f0cbaaeb57f685
Diptiingavale <127374765+Diptiingavale@users.noreply.github.com>
2023-03-19 00:29:28 +0530

    login done 2nd

--- javasaucedemo/src/pagelayer/Loginpage.java
+++ javasaucedemo/src/pagelayer/Loginpage.java
@@ -3,7 +3,7 @@ package pagelayer;
 public class Loginpage {
 
 	public static void main(String[] args) {
-		System.out.println(" I am in login page");
+		System.out.println(" I am in login page layer");
 
 	}
 
--- javasaucedemo/src/testpackage/Testclass.java
+++ javasaucedemo/src/testpackage/Testclass.java
@@ -5,7 +5,7 @@ public class Testclass {
 	public static void main(String[] args) {
 		System.out.println(" TESTcase CLASS");
 		System.out.println(" UTIL");
-		System.out.println(" POM.XML");
+		System.out.println(" POM.XMLss");
 	}
 
 }
